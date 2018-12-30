dc766fc0e791aec11f5ae548a875e35ec149555f
ismailraja <ismailraja@gmail.com>
2018-12-30 08:36:02 +0800

    Update DemoInnerMapping.sol

--- Mod02/DemoInnerMapping.sol
+++ Mod02/DemoInnerMapping.sol
@@ -1,6 +1,5 @@
 
 
-
 pragma solidity 0.4.24;
 
 contract DemoInnerMapping {
@@ -8,7 +7,7 @@ contract DemoInnerMapping {
     mapping (uint => mapping(address => string)) accountDetails;
     uint counter;
     
-    function addtoMapping(address addressDetails, bytes name) returns (uint)
+    function addtoMapping(address addressDetails, string name) returns (uint)
     {
         string memory names = string(name);
         counter = counter + 1;
@@ -17,13 +16,12 @@ contract DemoInnerMapping {
         return counter; 
     }
    
-   function getMappingMember(address addressDetails) returns (bytes)
+   function getMappingMember(address addressDetails) returns (string)
     {
         // 0xca35b7d915458ef540ade6068dfe2f44e8fa733c
-      return bytes( accountDetails[counter][addressDetails]);
+      return string( accountDetails[counter][addressDetails]);
     }
 }
-
 
 
 
